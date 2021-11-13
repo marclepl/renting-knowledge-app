c882929fa0765316a28f8fc88fdacff8c9d06ce2
Marc Leplattenier <marcol.lepl@gmail.com>
2021-11-13 15:25:01 +0100

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+#Creating the necessary routes for our mobile app (directing to different pages for the different functions)
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'pages#home'
+
+  get "profile", to: "pages#profile"
+
+  #having nested routes between books and bookings, as they're linked in the database
+  resources :books do
+    resources :bookings, only: [ :new, :create ]
+  end
 end
