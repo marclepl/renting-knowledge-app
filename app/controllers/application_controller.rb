cda10a0f2fc066813a3f23ba3e0de899b44cf580
Marc Leplattenier <marcol.lepl@gmail.com>
2021-11-13 15:53:47 +0100

    added links to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  #this forces people to authenticate before they can use all the app
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :city])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :city])
+  end
 end
