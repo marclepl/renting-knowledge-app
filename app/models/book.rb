3e2334a76c60ce6481d67e0d2ad8789a291918f3
Marc Leplattenier <marcol.lepl@gmail.com>
2021-11-19 15:13:25 +0100

    env file added to protect API key

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   #this allows us to make links between the models within our database
   belongs_to :user
   has_many :bookings
-  # has_one_attached :photo
+  has_one_attached :photo
 
   include PgSearch::Model
   multisearchable against: [:title, :author]
